57064b53e34c38c9a9001f8fccf18d7b78372238
adam tom <bplhp17@gmail.com>
2019-05-27 22:58:12 -0400

    Added hello form

--- name.py
+++ name.py
@@ -1,11 +1,12 @@
 
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/more")
-def more():
-    return render_template("more.html")\
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("nameField")
+    return render_template("hello.html", name=name)
